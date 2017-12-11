cac4219f74736f046d1d7774c5af03606a4b451f
Jack-X-Yang <1487019521@qq.com>
2017-12-11 22:25:19 +0800

    添加希尔排序

--- src/main/java/com/jack/learning/algorithms/sort/Shell.java
+++ src/main/java/com/jack/learning/algorithms/sort/Shell.java
@@ -1,5 +1,21 @@
 package com.jack.learning.algorithms.sort;
 
-public class Shell {
+public class Shell extends BaseSort {
+
+	@Override
+	public void sort(Comparable[] a) {
+		int N = a.length;
+		int h = 1;
+		while (h < N / 3)
+			h = h * 3 + 1;
+		while (h >= 1) {
+			for (int i = 0; i < N; i++) {
+				for (int j = i; j >= h && less(a[j], a[j - h]); j -= h) {
+					exch(a, j, j - h);
+				}
+			}
+			h = h / 3;
+		}
+	}
 
 }
---
+++ src/test/java/com/jack/learning/algorithms/sort/ShellTest.java
@@ -0,0 +1,20 @@
+package com.jack.learning.algorithms.sort;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ShellTest extends BaseSortTest {
+
+	@Before
+	public void setUp() throws Exception {
+		sortAlgorithms = new Shell();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
