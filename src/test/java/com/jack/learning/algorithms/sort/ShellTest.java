87a378200591a427bb2597f19f086df164f4632b
Jack-X-Yang <1487019521@qq.com>
2017-12-12 21:57:06 +0800

    把junit从3.8.1升级到4.12,删除测试中无用的依赖

--- src/test/java/com/jack/learning/algorithms/sort/ShellTest.java
+++ src/test/java/com/jack/learning/algorithms/sort/ShellTest.java
@@ -1,10 +1,7 @@
 package com.jack.learning.algorithms.sort;
 
-import static org.junit.Assert.*;
-
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Test;
 
 public class ShellTest extends BaseSortTest {
 
