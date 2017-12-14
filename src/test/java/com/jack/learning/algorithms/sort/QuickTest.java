3e792965a013e4fe14c05975642d0b190872a09f
Jack-X-Yang <1487019521@qq.com>
2017-12-14 22:24:33 +0800

    添加快速排序测试用例

---
+++ src/test/java/com/jack/learning/algorithms/sort/QuickTest.java
@@ -0,0 +1,15 @@
+package com.jack.learning.algorithms.sort;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class QuickTest extends BaseSortTest {
+
+	@Before
+	public void setUp() throws Exception {
+		sortAlgorithms = new Quick();
+	}
+
+}
