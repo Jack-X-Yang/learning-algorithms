afb0658ce3e10c822f8e7fd23346b95a3a3edd29
Jack-X-Yang <1487019521@qq.com>
2017-12-17 11:32:44 +0800

    添加通过最大堆实现的优先队列测试用例

---
+++ src/test/java/com/jack/learning/data/structure/priority/queue/MaxPqTest.java
@@ -0,0 +1,63 @@
+package com.jack.learning.data.structure.priority.queue;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MaxPqTest extends PriorityQueueTest {
+	MaxPQ pq = null;
+
+	@Before
+	public void setUp() throws Exception {
+		pq = new MaxPQ<Integer>(10);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Override
+	public void testIsEmpty() {
+		assertTrue(pq.isEmpty());
+
+		pq.insert(1);
+		assertFalse(pq.isEmpty());
+	}
+
+	@Override
+	public void testSize() {
+		assertEquals(pq.size(), 0);
+
+		pq.insert(1);
+		assertEquals(pq.size(), 1);
+	}
+
+	@Test
+	public void testInsert() {
+		int a = 1;
+		assertEquals(pq.size(), 0);
+
+		pq.insert(a);
+		assertEquals(pq.size(), 1);
+
+		int b = (Integer) pq.delMax();
+		assertEquals(a, b);
+	}
+
+	@Test
+	public void testDelMax() {
+		int[] a = { 1, 4, 2, 3, 5 };
+		int[] b = { 5, 4, 3, 2, 1 };
+
+		for (int c : a) {
+			pq.insert(c);
+		}
+
+		for (int i = 0; i < a.length; i++) {
+			assertEquals(pq.delMax(), b[i]);
+		}
+	}
+
+}
---
+++ src/test/java/com/jack/learning/data/structure/priority/queue/PriorityQueueTest.java
@@ -0,0 +1,25 @@
+package com.jack.learning.data.structure.priority.queue;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public abstract class PriorityQueueTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	abstract public void testIsEmpty();
+
+	@Test
+	abstract public void testSize();
+
+}
